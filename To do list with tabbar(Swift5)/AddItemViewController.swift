a7f4be474189b35f82965d9cdaded5effd1d2439
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-21 15:29:34 +0200

    UserDefaults is working

--- To do list with tabbar(Swift5)/AddItemViewController.swift
+++ To do list with tabbar(Swift5)/AddItemViewController.swift
@@ -7,14 +7,42 @@
 
 import UIKit
 
-class AddItemViewController: UIViewController {
+class AddItemViewController: UIViewController,UITextViewDelegate {
+    
+    let firstController = ToDoListViewController()
+    var items: [String] = []
+    
     
     
     @IBOutlet weak var addTextField: UITextField!
     
     @IBAction func addItebButton(_ sender: Any) {
+        
+        let  itemsObject = UserDefaults.standard.object(forKey:firstController.key)
+       
+        
+        if let tempItems = itemsObject as? [String]{
+            items = tempItems
+            guard let item = addTextField.text else {return}
+            items.append(item)
+        } else {
+            guard let item = addTextField.text else {return}
+            items = [item]
+        }
+        
+        
+        UserDefaults.standard.set(items, forKey:firstController.key)
+        
+        addTextField.text = ""
+        
+        
+        
+    
+        
+        
     }
     
+        
     
     
     
