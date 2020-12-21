2e8ea3c0c70262708b3186dff565370e690b9a41
SergeyGolenko <1989sergiotay@gmail.com>
2020-12-21 14:20:11 +0200

    add UserDefaults for FirstVC

--- To do list with tabbar(Swift5)/ToDoListViewController.swift
+++ To do list with tabbar(Swift5)/ToDoListViewController.swift
@@ -9,24 +9,38 @@ import UIKit
 
 class ToDoListViewController: UIViewController,UITableViewDelegate,UITableViewDataSource{
     
+    let key = "1111"
+    
+    var itemsToDo : [String] = ["bay eggs"]
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return itemsToDo.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = table.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-        cell.textLabel?.text = "4"
+        
+        let infoForCell = itemsToDo[indexPath.row]
+        cell.textLabel?.text = infoForCell
         return cell
     }
     
     
     @IBOutlet weak var table: UITableView!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadToDoItemsFromUserDefaults()
 
-        // Do any additional setup after loading the view.
+    }
+    
+    
+    func loadToDoItemsFromUserDefaults(){
+        if let arrayItems =  UserDefaults.standard.object(forKey: key) as? [String]{
+            itemsToDo = arrayItems
+        }
+        
     }
     
 
